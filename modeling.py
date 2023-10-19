4dad20831e2e0ae5c3c0c12228c28109db45a078
Jin Huang <kiyoshi@Jins-MacBook-Pro-2019.local>
2023-10-19 16:52:25 -0400

    Gaussian Naive Bayes

--- modeling.py
+++ modeling.py
@@ -3,7 +3,8 @@ import sys, os
 import numpy as np
 import matplotlib as plt
 import pandas as pd
-
+from math import sqrt, exp, pi
+from sklearn.naive_bayes import GaussianNB
 
 
 # Paths for result files
@@ -11,6 +12,9 @@ result_csv_path = "/afs/crc.nd.edu/group/cvrl/scratch_49/jhuang24/face_morph_dat
 model_name = "deepface"
 
 save_sample_path = "/afs/crc.nd.edu/group/cvrl/scratch_49/jhuang24/face_morph_data/sampled_data"
+
+data_dir = "/afs/crc.nd.edu/group/cvrl/scratch_49/jhuang24/face_morph_data/" \
+           "sampled_data/uniform_nb_train_3_ratio_0.8_tail_weight_0.4"
 
 
 # Set number of training frames as a parameter
@@ -145,20 +149,143 @@ def sampling(csv_path,
 
 
 
-def gaussian_naive_bayes(training_data,
-                         testing_data):
+# Calculate the mean of a list of numbers
+def mean(numbers):
+    return sum(numbers)/float(len(numbers))
+
+
+
+
+# Calculate the standard deviation of a list of numbers
+def stdev(numbers):
+    avg = mean(numbers)
+    variance = sum([(x-avg)**2 for x in numbers]) / float(len(numbers)-1)
+    return sqrt(variance)
+
+
+
+
+# Calculate the Gaussian probability distribution function for x
+def calculate_probability(x, mean, stdev):
+    exponent = exp(-((x-mean)**2 / (2 * stdev**2 )))
+    # print((1 / (sqrt(2 * pi) * stdev)) * exponent)
+    return (1 / (sqrt(2 * pi) * stdev)) * exponent
+
+
+
+
+def get_prob(data_frame,
+             classifier):
+    """
+
+    :param data_frame:
+    :return:
+    """
+    sample_w_frame = data_frame[["frame", "distance_to_A"]]
+    frames = sample_w_frame['frame'].unique()
+
+    print("Frames in: ", frames)
+
+    prob_dict = {}
+    prob_one_frame = []
+
+    for one_frame in frames:
+        frames = sample_w_frame.loc[sample_w_frame['frame'] == one_frame]
+        dist = frames["distance_to_A"].tolist()
+
+        for one_dist in dist:
+            prob = classifier.predict_proba(np.asarray(one_dist).reshape(-1, 1))
+            prob_one_frame.append(prob[0][1])
+
+        prob_dict[one_frame] = mean(prob_one_frame)
+
+    return prob_dict
+
+
+
+
+def gaussian_naive_bayes(data_dir):
     """
     Use class A and class B training data to fit a gaussian model,
     then use bayesian theorem to calculate probability.
 
-    :param data: training data for A and B, pandas DF
-    :param enrich_tail: True or False
     :return:
     """
+    # Step 1: Load training data for class A and B, and testing data
+    training_a = pd.read_csv(os.path.join(data_dir, "sample_a.csv"))
+    training_b = pd.read_csv(os.path.join(data_dir, "sample_b.csv"))
+    testing = pd.read_csv(os.path.join(data_dir, "test_samples.csv"))
 
+    # Take feature column for training data and convert data to list
+    a_sample = training_a[["distance_to_A"]]
+    b_sample = training_b[["distance_to_A"]]
+
+    a_sample = a_sample.distance_to_A.values.tolist()
+    b_sample = b_sample.distance_to_A.values.tolist()
+
+    print("Number of samples in class A:", len(a_sample))
+    print("Number of samples in class B:", len(b_sample))
+
+    # Step 2: Calculate mean and standard deviation
+    mean_a = mean(a_sample)
+    sd_a = stdev(a_sample)
+    var_a = np.var(np.asarray(a_sample))
+
+    mean_b = mean(b_sample)
+    sd_b = stdev(b_sample)
+    var_b = np.var(np.asarray(b_sample))
+
+    print("Mean for A: ", mean_a)
+    print("SD for A: ", sd_a)
+    print("Var for A: ", var_a )
+    print("Mean for B: ", mean_b)
+    print("SD for B :", sd_b)
+    print("Var for B: ", var_b)
+
+    """
+    # Ignore these for now 0 JH
+    
+    # Step 3: Gaussian Probability Density Function.
+        # -- defined above
+
+    # Step 4: Class Probabilities -- P(class|data) = P(X|class) * P(class)
+    # Calculate avg prob of choosing B for each frame, respectively
+    """
+
+    # Build and fit Gaussian Naive Bayes Model
+    clf = GaussianNB()
+
+    X = np.asarray(a_sample + b_sample)
+    Y = np.asarray([0] * len(a_sample) + [1] * len(b_sample))
+    clf.fit(X.reshape(-1, 1), Y)
+
+    # Training data A
+    prob_dict_a = get_prob(data_frame=training_a,
+                            classifier=clf)
+
+    # Training data B
+    prob_dict_b = get_prob(data_frame=training_b,
+                           classifier=clf)
+
+    # Testing data
+    prob_dict_test = get_prob(data_frame=testing,
+                         classifier=clf)
+
+    # Combine dictionaries and sort
+    prob_dict = {**prob_dict_a, **prob_dict_b, **prob_dict_test}
+    prob_dict = dict(sorted(prob_dict.items()))
+    print(prob_dict)
+
+
+
+
+def plot_prob_curve(prob_dict):
+    """
+
+    :param prob:
+    :return:
+    """
     pass
-
-
 
 
 
@@ -181,8 +308,10 @@ def evt_naive_bayes(training_data,
 
 
 if __name__ == "__main__":
-    sampling(csv_path=result_csv_path,
-             frame_index=all_frames,
-             nb_training=nb_training,
-             sampling_ratio=sampling_ratio,
-             sampling_method=sampling_method)
+    # sampling(csv_path=result_csv_path,
+    #          frame_index=all_frames,
+    #          nb_training=nb_training,
+    #          sampling_ratio=sampling_ratio,
+    #          sampling_method=sampling_method)
+
+    gaussian_naive_bayes(data_dir=data_dir)
