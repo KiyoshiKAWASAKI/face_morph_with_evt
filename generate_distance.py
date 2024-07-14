5b9c43e88b3fbaae58d8666910270858183b1328
Jin Huang <kiyoshi@Jins-MacBook-Pro-2019.local>
2024-07-14 14:50:22 -0500

    Generate distances for V4

--- generate_distance.py
+++ generate_distance.py
@@ -12,17 +12,9 @@ from scipy.spatial import distance
 
 # Paths
 feature_dir = "/project01/cvrl/jhuang24/face_morph_v4_5_sets_features/"
-save_csv_path = "/project01/cvrl/jhuang24/face_morph_v4_5_sets_dist/"
+save_csv_path = "/project01/cvrl/jhuang24/face_morph_v4_5_sets_dist"
 
 
-
-
-
-
-
-frames = [8, 20, 30, 40, 45, 50, 55, 60,
-          65, 70, 75, 80, 85,
-          90, 95, 103, 113, 121, 133, 140]
 person_A_ind = 1
 person_B_ind = 142
 
@@ -52,7 +44,6 @@ def check_features(feature_dir,
 
 
 
-
 def cosine_distance(feature_a,
                     feature_b):
     """
@@ -64,10 +55,13 @@ def cosine_distance(feature_a,
     return 1 - dot(feature_a, feature_b)/(norm(feature_a)*norm(feature_b))
 
 
+
+
 def euclidean_distance(feature_a,
                        feature_b):
 
     return distance.euclidean(feature_a, feature_b)
+
 
 
 
@@ -99,75 +93,73 @@ def process_feature_files(feature_dir,
 
 
 def gen_distance_csv(feature_dir,
-                     morph_names,
                      save_result_path,
-                     feature_ind,
                      person_a_ind,
-                     person_b_ind,
-                     nb_rows=6000):
+                     person_b_ind):
     """
 
     :param feature:
     :param save_result_path:
     :return:
     """
-    all_records = []
+    all_models = os.listdir(feature_dir)
+
+    """
+    print("All models: ", all_models)
+    
+    All models:  ['deepface', 'facenet', 'vgg_resnet', 'vgg_senet', 'vgg_vgg16']
+    """
 
     # For each morph, precess each frame
-    for one_morph in morph_names:
-        # Get person A and person B for this morph
-        person_A_feat_path = feature_dir + "/" + one_morph + "_morph_img_" + str(person_a_ind).zfill(4) + ".jpg.npy"
-        person_A_feat = np.load(person_A_feat_path, allow_pickle=True)
+    for one_model in all_models:
+        all_config = os.listdir(os.path.join(feature_dir, one_model))
+        # print(all_config) # ['enriched_tail', 'uniform']
 
-        person_B_feat_path = feature_dir + "/" + one_morph + "_morph_img_" + str(person_b_ind).zfill(4) + ".jpg.npy"
-        person_B_feat = np.load(person_B_feat_path, allow_pickle=True)
+        for one_config in all_config:
+            print("Dir: ", os.path.join(feature_dir, one_model, one_config))
 
-        for one_frame in feature_ind:
-            # For a frame, find its feature
-            feature_name = one_morph + "_morph_img_" + str(one_frame).zfill(4) + ".jpg.npy"
-            feature_path = os.path.join(feature_dir, feature_name)
+            all_feature = os.listdir(os.path.join(feature_dir, one_model, one_config))
 
-            # Load feature
-            feature = np.load(feature_path, allow_pickle=True)
+            all_records = []
+            for one_feature in all_feature:
+                """
+                # print(one_feature) # Aaron_Guiel_to_Andy_Graves_morph_img_0001.jpg.npy
+                # print(morph_name) # Aaron_Guiel_to_Andy_Graves
+                """
+                # Find name of this morph
+                morph_name = "_". join(one_feature.split(".")[0].split("_")[:-3])
+                frame = int(one_feature.split(".")[0].split("_")[-1])
+                feature = np.load(os.path.join(feature_dir, one_model, one_config, one_feature), allow_pickle=True)
 
-            # Computer distance to person A and person B
-            # dist_A = cosine_distance(person_A_feat, feature)
-            # dist_B = cosine_distance(person_B_feat, feature)
+                # Feature for A and B
+                person_A_feat_path = os.path.join(feature_dir, one_model) + \
+                                     "/uniform/" + morph_name + "_morph_img_0001.jpg.npy"
+                person_B_feat_path = os.path.join(feature_dir, one_model) + \
+                                     "/enriched_tail/" + morph_name + "_morph_img_0142.jpg.npy"
 
-            dist_A = euclidean_distance(person_A_feat, feature)
-            dist_B = euclidean_distance(person_B_feat, feature)
+                person_A_feat = np.load(person_A_feat_path, allow_pickle=True)
+                person_B_feat = np.load(person_B_feat_path, allow_pickle=True)
 
-            # print(dist_A, dist_B)
+                dist_A = euclidean_distance(person_A_feat, feature)
+                dist_B = euclidean_distance(person_B_feat, feature)
 
-            all_records.append([one_morph, one_frame, dist_A, dist_B])
+                all_records.append([morph_name, frame, dist_A, dist_B])
 
-    result_np = np.asarray(all_records)
+            result_np = np.asarray(all_records)
+            print("Number of records: ", result_np.shape[0])
 
-    assert result_np.shape[0] == nb_rows
-    # np.savetxt(save_result_path, result_np, delimiter=",", fmt="%s")
+            result_file_path = save_result_path + "/" + one_model + "_" + one_config + ".csv"
 
-    # with open(save_result_path, 'w') as myfile:
-    #     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-    #     wr.writerow(all_records)
+            df = pd.DataFrame(all_records)
+            df.to_csv(result_file_path)
 
-    df = pd.DataFrame(all_records)
-    df.to_csv(save_result_path)
-
-    print("Result file saved: ", save_result_path)
-
+            print("Result file saved: ", result_file_path)
 
 
 
 
 if __name__ == "__main__":
-    # check_features(feature_dir=feature_dir,
-    #                shape=size)
-
-    all_morphs = process_feature_files(feature_dir=feature_dir)
-
     gen_distance_csv(feature_dir=feature_dir,
-                     morph_names=all_morphs,
                      save_result_path=save_csv_path,
-                     feature_ind=frames,
                      person_a_ind=person_A_ind,
                      person_b_ind=person_B_ind)
